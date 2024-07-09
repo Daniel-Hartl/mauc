d046fee98884537221d0c16f43ad9b40485bc348
Daniel-Hartl <hartl123321@gmail.com>
2024-07-09 23:25:20 +0200

    comments...

--- Pulsmesser/app/src/main/java/com/example/pulsmesser/AudioPlayer.java
+++ Pulsmesser/app/src/main/java/com/example/pulsmesser/AudioPlayer.java
@@ -6,13 +6,35 @@ import java.util.Timer;
 import android.os.Handler;
 
 public class AudioPlayer {
-
+    /**
+     * states if the audioplayer is enabled.
+     */
     private boolean isEnabled;
+
+    /**
+     * the mediaplayer the audioPlayer wrapps.
+     */
     private MediaPlayer mediaPlayer;
+
+    /**
+     * the pulse.
+     */
     private int pulse;
+
+    /**
+     * the handler for the elapse of the timespan between the heartbeats.
+     */
     private Handler heartbeatHandler;
+
+    /**
+     * the runnable creating an interval between heartbeats.
+     */
     private Runnable heartbeatRunnable;
 
+    /**
+     * the constructor configuring the Handler and Runnable.
+     * @param context the mediaplayer initialized by the caller.
+     */
     public AudioPlayer(MediaPlayer context) {
         mediaPlayer = context;
         isEnabled = true;
@@ -34,29 +56,47 @@ public class AudioPlayer {
         };
     }
 
-
+    /**
+     * (Re)starts the audio after it has been paused and regives running permission.
+     */
     public void startAudio() {
         isEnabled = true;
         if (heartbeatHandler != null && heartbeatRunnable != null) {
             heartbeatHandler.post(heartbeatRunnable);
         }
     }
-    private long getHeartbeatInterval() {
 
-        // Berechne das Intervall basierend auf dem Pulswert
-        return 60000 / pulse; // Intervall in Millisekunden (60 Sekunden / bpm)
+    /**
+     * calculates the interval between two heartbeats
+     * @return the interval between heartbeats in ms
+     */
+    private long getHeartbeatInterval() {
+        return 60000 / pulse; // Interval (60sec / bpm)
     }
+
+    /**
+     * set the pulse and adjust the beat frequence.
+     * @param newPulse the new pulse as int.
+     */
     public void setPulse(int newPulse) {
         this.pulse = newPulse;
         if(pulse != 0)
             restartHeartbeatSimulation();
     }
+
+    /**
+     * Restarts the audio after it has been paused.
+     */
     private void restartHeartbeatSimulation() {
         if (heartbeatHandler != null && heartbeatRunnable != null) {
             heartbeatHandler.removeCallbacks(heartbeatRunnable);
             heartbeatHandler.post(heartbeatRunnable);
         }
     }
+
+    /**
+     * stops the heartbeat and removes running permission.
+     */
     public void stopAudio() {
         isEnabled = false;
         if (mediaPlayer != null) {
